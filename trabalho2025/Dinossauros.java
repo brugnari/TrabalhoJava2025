20bafc19316dba775f919d477f6a9a29f3ce7c52
ray.brugnari <ray.brugnari@gmail.com>
2025-03-18 00:12:11 -0300

    criando-objetos

--- trabalho2025/Dinossauros.java
+++ trabalho2025/Dinossauros.java
@@ -1,5 +1,8 @@
 public class Dinossauros {
     public static void main(String[] args) {
-        
+        Dinossauro[] Objetodinossauro = new Dinossauro[2];
+         Objetodinossauro[0] = new T_REX("T-REX", "5 toneladas", "50 km/h", 10, 100);
+         Objetodinossauro[1] = new Estegossauro("Estegossauro", "2 toneladas", "30 km/h", 15, 100);
+
     }
 }
