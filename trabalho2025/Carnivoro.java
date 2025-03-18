caa140aa8c74f5ca12def83195b7ce728072ae61
EduardoRuiiz <eduardoruiiz35@gmail.com>
2025-03-18 12:33:39 -0300

    Alguns detalhes alterados

--- trabalho2025/Carnivoro.java
+++ trabalho2025/Carnivoro.java
@@ -1,19 +1,22 @@
 
 public class Carnivoro extends Dinossauro implements Acoes {
+
     public Carnivoro(String nome, String peso, String velocidade, int idade, int saude) {
         super(nome, peso, velocidade, idade, saude);
     }
+
     @Override
     public void emitirSom() {
         System.out.println("O " + this.getNome() + " emitiu um som");
     }
+
     @Override
     public void mover() {
-        System.out.println("O "+this.getNome()+" corre a "+this.getVelocidade());
+        System.out.println("O " + this.getNome() + " corre a " + this.getVelocidade());
     }
-    
+
     @Override
     public void comer() {
-        System.out.println("O" +this.getNome()+" come carne.");
+        System.out.println("O " + this.getNome() + " come carne.");
     }
 }
--- trabalho2025/Main.java
+++ trabalho2025/Main.java
@@ -1,15 +1,17 @@
+
 public class Main {
+
     public static void main(String[] args) {
         Dinossauro[] dinossauros = new Dinossauro[3];
         dinossauros[0] = new Carnivoro("T-Rex", "5000kg", "50km/h", 65, 100);
-        dinossauros[1] = new Herbivoro("Triceratops", "8000kg", "30km/h", 70, 100);
-        dinossauros[2] = new Carnivoro("Velociraptor", "2000kg", "60km/h", 50, 100);
-        
+        dinossauros[1] = new Herbivoro("Triceratops", "8000kg", "20km/h", 70, 100);
+        dinossauros[2] = new Carnivoro("Velociraptor", "2000kg", "200km/h", 50, 100);
+
         for (Dinossauro dinossauro : dinossauros) {
+            dinossauro.Informacoes();
             dinossauro.emitirSom();
             dinossauro.mover();
             dinossauro.comer();
-            dinossauro.Informacoes();
             System.out.println();
         }
     }
